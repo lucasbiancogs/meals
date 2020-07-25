859c43600148e2ed6e68a9829bb23c95eb44d40e
lucasbiancogs <lucas.biancogs@hotmail.com>
2020-07-25 20:36:24 -0300

    Adding meal_item

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import '../models/category.dart';
+import '../components/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
@@ -23,7 +24,7 @@ class CategoriesMealsScreen extends StatelessWidget {
         body: ListView.builder(
           itemCount: categoryMeals.length,
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(categoryMeals[index]);
           },
         ));
   }
